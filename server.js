eff3011f921780dfa52a443896892d713f60a222
Luke Hendrick <lukehendrick41@gmail.com>
2017-07-21 18:14:05 -0500

    changed vars to const/let and moved to => functions

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-'use strict';
-
-var MongoClient = require('mongodb').MongoClient;
-var express = require('express');
-var app = express();
-var url = require('url');
-var uri = process.env.MONGOLAB_URI;
+const MongoClient = require('mongodb').MongoClient;
+const express = require('express');
+const app = express();
+const url = require('url');
+const uri = process.env.MONGOLAB_URI;
 let short;
-let seq = 1
 MongoClient.connect(uri, function(err, db) {
   if (err) {
     console.log('There was an error connecting to the database: ' + err);
@@ -15,25 +12,25 @@ MongoClient.connect(uri, function(err, db) {
 
     app.use(express.static('public'));
 
-    app.get("/", function (req, res) {
+    app.get("/", (req, res) => {
       res.sendFile(__dirname + '/views/index.html');
     })
-    app.get("/new/*", function (request, response) {
-      var reqDoc = {"original": request.params[0], "userId": Math.floor(1000 + Math.random() * 9000 )}
-      var userId = reqDoc.userId.toString();;
+    app.get("/new/*", (req, res) => {
+      let reqDoc = {"original": request.params[0], "userId": Math.floor(1000 + Math.random() * 9000 )}
+      let userId = reqDoc.userId.toString();;
       short.insertOne(reqDoc);
       response.send("Your new url is: " + request.get('host') + '/' + userId)
     });
     app.get("/:id", (req, res) => {
       let newId = Number(req.params.id)
       short.find({"userId": newId}).toArray(function (err, docs) {
-        var redirect = docs[0].original
+        let redirect = docs[0].original
         res.redirect(redirect)
       })
     })
     console.log('Success!');
     let port = process.env.PORT || 3000;
-    var listener = app.listen(port, function () {
+    let listener = app.listen(port, function () {
         console.log('Your app is listening on port ' + listener.address().port);
     });
   }
