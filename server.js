6f01edba3a5478036177ad08537c84b37298e7de
LukeHendrick <lukehendrick41@gmail.com>
2017-07-21 22:17:24 -0500

    changed express route to avoid conflict

--- server.js
+++ server.js
@@ -16,15 +16,19 @@ MongoClient.connect(uri, function(err, db) {
       res.sendFile(__dirname + '/views/index.html');
     })
     app.get("/new/*", (req, res) => {
-      let reqDoc = {"original": req.params[0], "userId": Math.floor(1000 + Math.random() * 9000 )}
-      let userId = reqDoc.userId.toString();;
+      var reqDoc = {"original": req.params[0], "userId": Math.floor(1000 + Math.random() * 9000 )}
+      var userId = reqDoc.userId.toString();;
       short.insertOne(reqDoc);
       res.send("Your new url is: " + req.get('host') + '/' + userId)
     });
-    app.get("/:id", (req, res) => {
-      let newId = Number(req.params.id)
+    app.get("/short/:id", (req, res) => {
+      var newId = Number(req.params.id)
       short.find({"userId": newId}).toArray(function (err, docs) {
-        let redirect = docs[0].original
+        if (docs === undefined) {
+          res.send('Entry not found!')
+          return;
+        }
+        var redirect = docs[0].original
         res.redirect(redirect)
       })
     })
